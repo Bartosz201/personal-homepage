30dd3b4d55ef43b94e3f787e19d8d899001b16c3
Bartosz Barc <bartosz.barc721@gmail.com>
2023-03-29 15:29:25 +0200

    Changed image in Header

--- src/features/personal-homepage/Header/index.js
+++ src/features/personal-homepage/Header/index.js
@@ -1,5 +1,5 @@
 import { Image, Paragraph, StyledH1, StyledHeader, } from "./styled";
-import exampleImage from "../images/exampleImage.jpg"
+import myImage from "../images/Bartosz.jpg"
 import { ImportantText } from "../ImportantText";
 import EmailLink from "../EmailLink";
 import { useSelector } from "react-redux";
@@ -10,12 +10,12 @@ const Header = () => {
 
     return (
         <StyledHeader>
-            <Image src={exampleImage} alt="" />
+            <Image src={myImage} alt="" />
                 <div>
                 <ImportantText>THIS IS</ImportantText>
                 <StyledH1 themeIsDark={themeIsDark}>Bartosz Barć</StyledH1>
                 <Paragraph>
-                    I'm a starting front end developer. I am searching for a job as a junior front-end developer, where I will have the opportunity to develop and gain professional experience.
+                    I'm a starting front end developer. I'm searching for a job as a junior front-end developer, where I will have the opportunity to develop and gain professional experience.
                 </Paragraph>
                 <EmailLink />
                 </div>
